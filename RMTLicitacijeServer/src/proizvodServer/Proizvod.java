a2c78ac6809904042eb677765bc8f1e473129bd4
Luka Radovanovic <lukadlm97@gmail.com>
2019-06-18 23:08:28 +0200

    Dodata nova klasa Proizvod

---
+++ RMTLicitacijeServer/src/proizvodServer/Proizvod.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proizvodServer;
+
+/**
+ *
+ * @author Luka
+ */
+public class Proizvod {
+    public int IDProizvoda;
+    public String nazivProizvoda;
+
+    @Override
+    public String toString() {
+        return "Proizvod{" + "IDProizvoda=" + IDProizvoda + ", nazivProizvoda=" + nazivProizvoda + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Proizvod other = (Proizvod) obj;
+        if (this.IDProizvoda != other.IDProizvoda) {
+            return false;
+        }
+        return true;
+    }
+
+    public void setIDProizvoda(int IDProizvoda) {
+        this.IDProizvoda = IDProizvoda;
+    }
+
+    public void setNazivProizvoda(String nazivProizvoda) {
+        this.nazivProizvoda = nazivProizvoda;
+    }
+
+    public int getIDProizvoda() {
+        return IDProizvoda;
+    }
+
+    public String getNazivProizvoda() {
+        return nazivProizvoda;
+    }
+
+    public Proizvod(int IDProizvoda, String nazivProizvoda) {
+        this.IDProizvoda = IDProizvoda;
+        this.nazivProizvoda = nazivProizvoda;
+    }
+
+    public Proizvod() {
+    }
+    
+}
