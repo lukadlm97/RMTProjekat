ac418f6f6939238f4c36ee774018a2f23a094058
Luka Radovanovic <lukadlm97@gmail.com>
2019-06-19 11:21:02 +0200

    Dodata Server logika sa Thread-om

---
+++ RMTLicitacijeServer/src/server/Server.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Luka
+ */
+public class Server {
+    static ServerNit klijenti[] = new ServerNit[20];
+    
+    public static void main(String[] args) {
+           int port = 22272;
+           Socket klijentSoket = null;
+           
+        try {
+            ServerSocket serverSoket = new ServerSocket(port);
+            while(true){
+                System.out.println("Cekanje klijenta!");
+                klijentSoket = serverSoket.accept();
+                System.out.println("Klijent konektovan");
+                for(int i=0;i<klijenti.length;i++){
+                    if(klijenti[i] == null){
+                        klijenti[i] = new ServerNit(klijentSoket);
+                        klijenti[i].start();
+                        break;
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+           
+           
+          
+    }
+}
---
+++ RMTLicitacijeServer/src/server/ServerNit.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Luka
+ */
+public class ServerNit extends Thread{
+    BufferedReader ulazniTokOdKlijenta = null;
+    PrintStream izlazniTokKaKlijentu = null;
+    Socket soketZaKomunikaciju = null;
+
+    public ServerNit(Socket klijentSoket) {
+        this.soketZaKomunikaciju = klijentSoket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            ulazniTokOdKlijenta = new BufferedReader(new InputStreamReader(soketZaKomunikaciju.getInputStream()));
+            izlazniTokKaKlijentu = new PrintStream(soketZaKomunikaciju.getOutputStream());
+        } catch (IOException ex) {
+            Logger.getLogger(ServerNit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+}
